d3e67aed6a40b0f5e3b1a17347fae36f344c72d2
Eduarda Noronha <eduardadenoronha8@gmail.com>
2025-08-24 18:56:36 -0300

    Adicionando parte do código que transforma o csv em um df do pandas para inserir os dados na tabela do SQL.

--- baixar_dados_abertos.py
+++ baixar_dados_abertos.py
@@ -19,7 +19,6 @@ data_mes = data_atual.strftime("%Y%m")
 print('Programa iniciado...')
 
 # Estabelecer a conexão com o banco de dados do SQL
-print(pyodbc.drivers())
 conn = pyodbc.connect(
     "DRIVER={PostgreSQL ODBC Driver(UNICODE)};"
     "SERVER=localhost;"
@@ -39,22 +38,86 @@ url = f"https://dados.cvm.gov.br/dados/FI/DOC/INF_DIARIO/DADOS/inf_diario_fi_{da
 # Pasta de destino para salvar o arquivo
 diretorio = r"C:\Users\Duda\PycharmProjects\ETL_dados_diarios_fundos_cvm/arquivos_zip"
 
-# Nome do arquivo zip
+#Nome do arquivo zip
 zip_name = os.path.join(diretorio, f"inf_diario_fi_{data_mes}.zip")
-
-# Nome dos arquivos CSV dentro do zip
+#Nome do arquivo CSV dentro do zip
 csv = fr"inf_diario_fi_{data_mes}.csv"
+#caminho completo do csv
+caminho_csv = os.path.join(diretorio, csv)
 
 # Baixar o arquivo zip
-urllib.request.urlretrieve(url, zip_name)
-print("Arquivo ZIP baixado com sucesso!")
-# Extrair os arquivos CSV do zip
-with zipfile.ZipFile(zip_name, "r") as zip_ref:
-    zip_ref.extract(csv, diretorio)
-os.remove(zip_name)
-print("Arquivos CSV extraídos do ZIP com sucesso!")
+#urllib.request.urlretrieve(url, zip_name)
+#print("Arquivo ZIP baixado com sucesso!")
+## Extrair os arquivos CSV do zip
+#with zipfile.ZipFile(zip_name, "r") as zip_ref:
+#    zip_ref.extract(csv, diretorio)
+#os.remove(zip_name)
+#print("Arquivos CSV extraído do ZIP com sucesso!")
 
 
+#Transforma o CSV em um dataframe pandas
+df_fundo = pd.read_csv(caminho_csv, encoding="cp1252", delimiter=";")
+df_fundo = df_fundo.fillna("")
+df_fundo.columns = [
+    "TP_FUNDO_CLASSE"
+    ,"CNPJ_FUNDO_CLASSE"
+    ,"ID_SUBCLASSE"
+    ,"DT_COMPTC"
+    ,"VL_TOTAL"
+    ,"VL_QUOTA"
+    ,"VL_PATRIM_LIQ"
+    ,"CAPTC_DIA"
+    ,"RESG_DIA"
+    ,"NR_COTST"
+]
+
+print('Arquivo acessado: ', caminho_csv)
+print(f"Os dados do arquvo registro_fundo.csv serão inseridos na tabela dados_abertos_cvm.informe_diario_fundos...")
+
+count_linhas_insert = 0
+
+#Itera sobre os valores do dataframe para inserir os dados na tabela
+for index, row in df_fundo.iterrows():
+    TP_FUNDO_CLASSE = row["TP_FUNDO_CLASSE"] if pd.notna(row["TP_FUNDO_CLASSE"]) else None
+    CNPJ_FUNDO_CLASSE = row["CNPJ_FUNDO_CLASSE"] if pd.notna(row["CNPJ_FUNDO_CLASSE"]) else None
+    ID_SUBCLASSE = row["ID_SUBCLASSE"] if pd.notna(row["ID_SUBCLASSE"]) else None
+    DT_COMPTC =  row["DT_COMPTC"]
+    VL_TOTAL = pd.to_numeric(row["VL_TOTAL"], errors="coerce")
+    VL_QUOTA = pd.to_numeric(row["VL_QUOTA"], errors="coerce")
+    VL_PATRIM_LIQ = pd.to_numeric(row["VL_PATRIM_LIQ"], errors="coerce")
+    CAPTC_DIA = pd.to_numeric(row["CAPTC_DIA"], errors="coerce")
+    RESG_DIA = pd.to_numeric(row["RESG_DIA"], errors="coerce")
+    NR_COTST = pd.to_numeric(row["NR_COTST"], errors="coerce")
+
+    #Insere os dados na tabela
+    query_insert = f"""
+    INSERT INTO dados_abertos_cvm.informe_diario_fundos
+    VALUES (
+    '{TP_FUNDO_CLASSE}'
+    ,'{CNPJ_FUNDO_CLASSE}'
+    ,case when '{ID_SUBCLASSE}' = '' THEN 'Não se aplica' else '{ID_SUBCLASSE}' END
+    ,'{DT_COMPTC}'
+    ,'{VL_TOTAL}'
+    ,'{VL_QUOTA}'
+    ,'{VL_PATRIM_LIQ}'
+    ,'{CAPTC_DIA}'
+    ,'{RESG_DIA}'
+    ,{NR_COTST}
+    )
+    """
+    try:
+        #print(query_insert)
+        cursor.execute(query_insert)
+        conn.commit()
+        count_linhas_insert +=1
+
+        print("Dados inseridos na tabela dados_abertos_cvm.informe_diario_fundos com sucesso!")
+
+    except Exception as e:
+        print(e)
+
+
+print(f"Total de linhas inseridas: {count_linhas_insert}")
 final = time.time()
 tempo_execucao = final-inicio
 print("Programa finalizado!")
